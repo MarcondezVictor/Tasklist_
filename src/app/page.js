23b760606e517e4e88be85a987793853b0e5bb58
Joao Silva <jotal96@gmail.com>
2026-05-05 11:35:37 +0100

    adicionada variável de estado

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,30 @@
-import Image from "next/image";
+'use client';
+
 import styles from "./page.module.css";
+import {useState} from "react";
 
 export default function Home() {
-  return (
+    // variável de estado
+    const [name, setName] = useState("mundo!");
+
+    // função
+    const alterName =
+        () => { setName("mundo!")}
+
+    return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h2>Olá {name}</h2>
 
+          <button style={{width: '100px', fontSize: '16px'}} onClick={
+              () => {setName("MUNDO!!!!")}
+          }>
+              Clica em mim ;)
+          </button>
 
+          <button style={{width: '100px', fontSize: '16px'}} onClick={alterName}>
+              Clica em mim antes :D
+          </button>
       </main>
     </div>
   );
