9428d48503121ce8dbf5755f6d2c295fff7209dc
Joao Silva <jotal96@gmail.com>
2026-05-12 09:50:07 +0100

    alterado estilo global

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +20,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+      <ul>
+        <li><Link href={"/"}>Home</Link></li>
+        <li><Link href={"/lista"}>Lista</Link></li>
+      </ul>
+      <div className={'mainPage'}>
+      {children}
+      </div>
+      </body>
     </html>
   );
 }
---
+++ src/app/lista/page.js
@@ -0,0 +1,7 @@
+export default function Lista(){
+    return <div>
+        <main>
+            Olá Página Lista
+        </main>
+    </div>;
+}
