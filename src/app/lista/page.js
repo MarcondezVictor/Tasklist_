e9044a566ffb0f47ad79b49ad4e0475d32e06ced
Joao Silva <jotal96@gmail.com>
2026-05-12 10:35:11 +0100

    adicionado estilo

--- src/app/lista/page.js
+++ src/app/lista/page.js
@@ -25,6 +25,7 @@ export default function Lista() {
 
     return <div>
         <main>
+            <h2 style={{color: 'lightcyan'}}>Lista de tarefas</h2>
             <div className={styles.displayFlex}>
                 <label>Escreve no input a tarefa que queres realizar</label>
                 <div className={styles.divInputBtn}>
