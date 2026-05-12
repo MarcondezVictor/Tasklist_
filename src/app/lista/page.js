e98cc00e0ca207ceb6ab927cabd1d70972bf0831
Joao Silva <jotal96@gmail.com>
2026-05-12 10:34:24 +0100

    nova página com lista de tarefas

--- src/app/lista/page.js
+++ src/app/lista/page.js
@@ -1,7 +1,46 @@
-export default function Lista(){
+'use client'
+
+import styles from "./page.module.css";
+import {useState} from "react";
+
+export default function Lista() {
+    /*
+        Variáveis de estado
+     */
+    const [inputTexto, setInputTexto] = useState("");
+    const [lista, setLista] = useState([]);
+
+    /*
+        Funções
+     */
+    const handleButtonClick = () => {
+        // copio a variavel de estado
+        var copiaLista = [...lista];
+        // atualizo a copia da lista
+        copiaLista.push(inputTexto);
+        // por fim atualizo a lista
+        setLista(copiaLista);
+    }
+
+
     return <div>
         <main>
-            Olá Página Lista
+            <div className={styles.displayFlex}>
+                <label>Escreve no input a tarefa que queres realizar</label>
+                <div className={styles.divInputBtn}>
+                    <input value={inputTexto}
+                           onChange={
+                            (e)=>
+                            {setInputTexto(e.target.value)}
+                    }
+                           className={styles.inputLista}/>
+                    <button onClick={()=>{handleButtonClick();}}
+                            className={styles.botaoLista}>Adiciona à lista de tarefas</button>
+                </div>
+                    {lista.map((entrada)=> {
+                        return <p>{entrada}</p>;
+                    })}
+            </div>
         </main>
     </div>;
 }
